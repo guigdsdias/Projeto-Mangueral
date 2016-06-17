277db2c869fba2727fc4f8c905d4df3f91cdc6b6
Fernando Ramalho Cangussu <frcangussu@gmail.com>
2016-06-17 18:55:24 -0300

    implementando home

--- modulos/classificados/spa/pages/home/home-controller.js
+++ modulos/classificados/spa/pages/home/home-controller.js
@@ -24,8 +24,17 @@
 
 			for (var i=0; i<response.data.length;i){
 				vm.listaCategoria.push(response.data.slice(i,i+4));
+
+				// preenche a lista com elementos vazios no final para completar 4 elementos por linha
+				var ultimoInserido = vm.listaCategoria[vm.listaCategoria.length-1];
+				for (var j=ultimoInserido.length;j<4;j++){
+					ultimoInserido.push({});
+				}
+
 				i +=4;
 			}
+
+			console.log(vm.listaCategoria);
 
 		});
 	}
