d0c021257b55f4d0842a7bfaacd6c8f6940a4d87
Fernando Ramalho Cangussu <frcangussu@gmail.com>
2016-06-17 18:27:04 -0300

    implementando home

--- modulos/classificados/angular/router.js
+++ modulos/classificados/angular/router.js
@@ -13,14 +13,14 @@ angular.module("pmApp", [
 .config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
-                //when('/home',         {templateUrl: 'spa/pages/home/home.html',                 controller: 'homeController as vm'}).
-                when('/subcategoria', {templateUrl: 'spa/pages/subcategoria/sub-categoria.html', controller: 'subCategoriaController as vm'}).
-                when('/carossel',     {templateUrl: 'spa/includes/home/carousel.html', controller: 'carouselController as vm'}).
+                when('/home',         {templateUrl: 'spa/pages/home/home.html'}).
+                // when('/subcategoria', {templateUrl: 'spa/pages/subcategoria/sub-categoria.html', controller: 'subCategoriaController as vm'}).
+                // when('/carossel',     {templateUrl: 'spa/includes/home/carousel.html', controller: 'carouselController as vm'}).
 //                when('/cadastrese', {templateUrl: 'pages/cadastrese.html',       controller: 'portalController'}).
 //                when('/create', {templateUrl: 'create.html', controller: 'portalController'}).
 //                when('/delete', {templateUrl: 'list.html', controller: 'portalController'}).
                 otherwise({
-                    redirectTo: '/carossel'
+                    redirectTo: '/home'
                 });
     }
 ])
--- modulos/classificados/spa/pages/home/home-controller.js
+++ modulos/classificados/spa/pages/home/home-controller.js
@@ -5,17 +5,29 @@
 
 	.controller('homeController', homeController);
 
-	homeController.$inject = ['$http','$rootScope','$scope','TesteService'];
+	homeController.$inject = ['$http','$rootScope','$scope','TesteService','PARAMS'];
 
-	function homeController($http, $rootScope, $scope, TesteService){
+	function homeController($http, $rootScope, $scope, TesteService, PARAMS){
 
 		var vm = this;
 
-		vm.teste = "Teste homeController";
+		vm.includes = PARAMS.index.includes;
 
-		vm.uf = TesteService.get();
+		$http({
+			url: "/apirest/admin/categoria",
+			method: "GET"
+		}).then(function(response){
+			vm.listaCategoria = [];
 
-		console.log(vm.uf);
+			vm.primeiros = response.data.splice(0,4);
+			// vm.listaCategoria = response.data;
+
+			for (var i=0; i<response.data.length;i){
+				vm.listaCategoria.push(response.data.slice(i,i+4));
+				i +=4;
+			}
+
+		});
 	}
 
 })();
