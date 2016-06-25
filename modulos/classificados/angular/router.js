eaa2abb3066c0c84649c9b85561f1cb8d77b084d
Fernando Ramalho Cangussu <frcangussu@gmail.com>
2016-06-25 13:41:56 -0300

    removendo pm-services e adicionando services do admin

--- modulos/classificados/angular/router.js
+++ modulos/classificados/angular/router.js
@@ -3,7 +3,7 @@ angular.module("pmApp", [
 	'kendo.directives',
 	'pmDirectives',
 	'pmConstants',
-    'pmServices',
+    'services',
     'moduloIndexController',
     'moduloHomeController',
     'moduloCarouselController',
